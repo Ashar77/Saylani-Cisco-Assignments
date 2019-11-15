614450d2d2951a030a0361200e8a5f77e2b968e5
Muhammad Ashar Mehmood <50556125+Ashar77@users.noreply.github.com>
2019-11-15 04:10:48 -0800

    Add files via upload
    Assignment#2 solution

---
+++ assignment 2.py
@@ -0,0 +1,120 @@
+# # Q6
+# a = {1,1,2,3,5,8,13,21,34,55,89}
+# for i in a:
+#     if i <5:
+#         print(i)
+
+
+# #Q4
+# lst = []
+# number_of_inputs = int(input('enter number of elements in list: '))
+# for i in range(number_of_inputs):
+#     element = int(input('enter element: '))
+#     lst.append(element)
+
+# sum=0
+# for j in lst:
+#     sum+=j
+
+# print(sum)
+
+
+
+
+#Q3
+# list2 = [1,2,4,8,'1']
+# print(list2)
+# length = 0
+# for i in list2:
+#     length+=1
+
+# print(length)
+
+
+
+#Q2
+
+# num = int(input('enter number to check: '))
+
+# if num%2 ==0:
+#     print('even')
+# else:
+#     print('odd')
+
+
+#Q5
+# lst = []
+# number_of_inputs = int(input('enter number of elements in list: '))
+# for i in range(number_of_inputs):
+#     element = int(input('enter element: '))
+#     lst.append(element)
+
+# lst2 = lst.copy()
+# largest = 0
+
+# for i in range(len(lst)):
+#     for j in range(len(lst2)):
+#         if lst[i] > lst2[j]:
+#             largest = lst[i]
+        
+
+# print('largest number is: ',largest)
+
+
+
+
+
+#q1
+
+# print('enter marks of of 100: ')
+# python = int(input('python: '))
+# java = int(input('java: '))
+# js = int(input('js: '))
+# c = int(input('c: '))
+# swift = int(input('swift: '))
+
+# def grade(marks):
+#     if marks>90:
+#         return 'A'
+#     elif marks > 80:
+#         return 'B'
+#     elif marks > 70:
+#         return 'C'
+#     elif marks > 60:
+#         return 'D'
+#     else:
+#         return 'F'
+
+# print('marks in python',python,grade(python))
+
+# print('marks in python',java,grade(java))
+# print('marks in python',js,grade(js))
+# print('marks in python',c,grade(c))
+# print('marks in python',swift,grade(swift))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
