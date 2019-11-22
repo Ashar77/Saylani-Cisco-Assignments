8d05de63ea76ce6dd13626e961e06bcb955effb6
Muhammad Ashar Mehmood <50556125+Ashar77@users.noreply.github.com>
2019-11-22 22:44:23 +0500

    Add files via upload
    assignment#1

---
+++ ash1.py
@@ -0,0 +1,25 @@
+print("twinkle twinkle little star ,")
+print("      ","how i wonder what you are")
+print("               ","up above the world so high")
+print("               ","like a diamond in the sky")
+print("twinkle twinkle little star ,")
+print("      ","how i wonder what you are")
+from datetime import date
+print(date.today( ))
+radius=int(input("enter radius of circle...."))
+sq=radius**2
+print(3.14**sq)
+a=input("enter name")
+l=a.split( )
+l.insert(1," ")
+l.reverse( )
+ss=" "
+for v in l:
+    ss=ss+  v
+print(ss)
+s=int(input("enter.."))
+v=int(input("enter.."))
+print(s+v)
+import sys
+print(sys.version)
+
